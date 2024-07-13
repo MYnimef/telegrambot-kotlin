85ed1c254601c4331a74fe92a30212c1a186af13
Ivan <73215129+MYnimef@users.noreply.github.com>
2024-07-13 21:37:30 +0300

    final update

--- src/main/kotlin/com/mynimef/bot/MainApplication.kt
+++ src/main/kotlin/com/mynimef/bot/MainApplication.kt
@@ -1,13 +1,5 @@
 package com.mynimef.bot
 
-import com.mynimef.bot.config.BotCreator
-
 
 internal fun main() {
-
-    val bot = BotCreator(token = "insert_your_token_here")
-        .addCommands(commandsClass = MyBotCommands())
-        .addCallbacks(builder = MyCallbacksBuilder())
-        .start()
-
 }
